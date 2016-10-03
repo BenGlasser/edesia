148a792455ecffa6ebeb51585298ffa2c993f028
Ben Glasser <ben@benglasser.com>
2016-10-02 19:24:10 -0600

    set up project structure

---
+++ src/app/components/meal/meal.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { MealComponent } from './meal.component';
+
+describe('Component: Meal', () => {
+  it('should create an instance', () => {
+    let component = new MealComponent();
+    expect(component).toBeTruthy();
+  });
+});
